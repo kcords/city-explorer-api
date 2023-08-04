7e94a98a0f2da3c383cc97a465e81f09d637ac5b
Kyle Cordell <kcordell-git@outlook.com>
2023-08-03 23:03:38 -0700

    Adds new movies route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { getCityForecast } = require('./forecast')
+const { getTopMovies } = require("./movies");
 
 const app = express();
 
@@ -11,21 +12,33 @@ app.use(cors());
 
 const { PORT } = process.env || { PORT: 3002 };
 
-app.get('/', (req, res, next) => {
-  res
-    .status(200)
-    .send('Default route is working!')
-})
+app.get("/", (req, res, next) => {
+  res.status(200).send("Default route is working!");
+});
 
-app.get("/weather", async ({ query }, res, next) => {
+app.get("/weather", async ({ query: { searchQuery, lat, lon } }, res, next) => {
   try {
-    const { searchQuery: city_name, lat, lon } = query;
-    const cityForecast = await getCityForecast({ city_name, lat, lon });
+    const cityForecast = await getCityForecast({ lat, lon });
 
     if (!cityForecast) {
-      throw new Error(`No forecast found for the requested city ${city_name}`);
+      throw new Error(
+        `No forecast found for the requested city ${searchQuery}`
+      );
     }
     res.status(200).send(cityForecast);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/movies", async ({ query: { searchQuery } }, res, next) => {
+  try {
+    const topMovies = await getTopMovies({ searchQuery });
+
+    if (!topMovies) {
+      throw new Error(`No movies found for the requested city ${searchQuery}`);
+    }
+    res.status(200).send(topMovies);
   } catch (error) {
     next(error);
   }
