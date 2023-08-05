b3e0d10db0315ae7d183f0a6153b61c37d04ad39
Kyle Cordell <kcordell-git@outlook.com>
2023-08-05 12:17:29 -0700

    Refactors movies module to simplify

--- modules/movies.js
+++ modules/movies.js
@@ -1,26 +1,22 @@
 "use strict";
 
-const axios = require("axios");
+const axios = require("./axiosInstance");
 
 const MOVIE_API_BASE_URL = "https://api.themoviedb.org/3/search/movie";
 const MOVIE_IMG_BASE_URL = "https://image.tmdb.org/t/p/original";
 
-const getTopMovies = async ({ searchQuery }) => {
+const getTopMovies = ({ query: { searchQuery } }, res, next) => {
   const queryUrl = `${MOVIE_API_BASE_URL}?page=1&sort_by=popularity.desc&api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
 
-  const {
-    data: { results: movieData },
-  } = await axios(queryUrl);
-
-  const standardizedMovies = movieData
-    ? standardizeMovieData(movieData)
-    : undefined;
-
-  return standardizedMovies;
+  axios(queryUrl)
+    .then(({ data }) => {
+      if (data?.length > 0) return data;
+      throw new Error(`No movies found for ${searchQuery}`);
+    })
+    .then((data) => data.map((movie) => new Movie(movie)))
+    .then((formattedData) => res.status(200).send(formattedData))
+    .catch((error) => next(error));
 };
-
-const standardizeMovieData = (movieData) =>
-  movieData.map((movie) => new Movie(movie));
 
 class Movie {
   constructor({
@@ -36,7 +32,7 @@ class Movie {
     this.overview = overview;
     this.average_votes = vote_average;
     this.total_votes = vote_count;
-    this.image_url = poster_path ? `${MOVIE_IMG_BASE_URL}${poster_path}` : '';
+    this.image_url = poster_path ? `${MOVIE_IMG_BASE_URL}${poster_path}` : "";
     this.popularity = popularity;
     this.released_on = release_date;
   }
