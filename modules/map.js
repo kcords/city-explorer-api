206bcf01e8d1f36341fad5ee7f1ebfa6a4f407a0
Kyle Cordell <kcordell-git@outlook.com>
2023-08-09 19:42:45 -0700

    Updates weather, map and movies to utilize the new cache

--- modules/map.js
+++ modules/map.js
@@ -1,22 +1,34 @@
 "use strict";
 
 const axios = require("./axiosInstance");
+const { cache, getTTL } = require("./cache.js");
 
 const MAP_API_BASE_URL = "https://maps.locationiq.com/v3/staticmap";
 
-const getAreaMap = ({ query: { searchQuery, lat, lon } }, res, next) => {
+const getMap = ({ query: { searchQuery, lat, lon } }, res, next) => {
+  const key = `map-${searchQuery}-${lat}-${lon}`;
   const queryUrl = `${MAP_API_BASE_URL}?key=${process.env.MAP_API_KEY}&center=${lat},${lon}&zoom=12&scale=2`;
 
-  axios(queryUrl, {
-    responseType: "arraybuffer",
-    headers: { Referer: process.env.MAP_REFERER },
-  })
-    .then(({ data }) => {
-      if (data) return data;
-      throw new Error(`No map found for ${searchQuery}`);
+  res.status(200).type("image/jpeg");
+
+  if (cache.has(key)) {
+    res.send(cache.get(key));
+  } else {
+    axios(queryUrl, {
+      responseType: "arraybuffer",
+      headers: { Referer: process.env.MAP_REFERER },
     })
-    .then((data) => res.status(200).type("image/jpeg").send(data))
-    .catch((error) => next(error));
+      .then(({ data }) => {
+        if (data) return data;
+        throw new Error(`No map found for ${searchQuery}`);
+      })
+      .then((data) => {
+        const ttl = getTTL.daysLater(30);
+        cache.set(key, data, { ttl });
+        res.send(data);
+      })
+      .catch((error) => next(error));
+  }
 };
 
-module.exports = { getAreaMap };
+module.exports = { getMap };
--- modules/movies.js
+++ modules/movies.js
@@ -1,21 +1,33 @@
 "use strict";
 
 const axios = require("./axiosInstance");
+const { cache, getTTL } = require("./cache.js");
 
 const MOVIE_API_BASE_URL = "https://api.themoviedb.org/3/search/movie";
 const MOVIE_IMG_BASE_URL = "https://image.tmdb.org/t/p/original";
 
-const getTopMovies = ({ query: { searchQuery } }, res, next) => {
+const getMovies = ({ query: { searchQuery } }, res, next) => {
+  const key = `movies-${searchQuery}`;
   const queryUrl = `${MOVIE_API_BASE_URL}?page=1&sort_by=popularity.desc&api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
 
-  axios(queryUrl)
-    .then(({ data: { results: movieData } }) => {
-      if (movieData?.length > 0) return movieData;
-      throw new Error(`No movies found for ${searchQuery}`);
-    })
-    .then((movieData) => movieData.map((movie) => new Movie(movie)))
-    .then((formattedMovieData) => res.status(200).send(formattedMovieData))
-    .catch((error) => next(error));
+  res.status(200);
+
+  if (cache.has(key)) {
+    res.send(cache.get(key));
+  } else {
+    axios(queryUrl)
+      .then(({ data: { results: movieData } }) => {
+        if (movieData?.length > 0) return movieData;
+        throw new Error(`No movies found for ${searchQuery}`);
+      })
+      .then((movieData) => movieData.map((movie) => new Movie(movie)))
+      .then((formattedMovieData) => {
+        const ttl = getTTL.endOfDay();
+        cache.set(key, formattedMovieData, { ttl });
+        res.send(formattedMovieData);
+      })
+      .catch((error) => next(error));
+  }
 };
 
 class Movie {
@@ -38,4 +50,4 @@ class Movie {
   }
 }
 
-module.exports = { getTopMovies };
+module.exports = { getMovies };
